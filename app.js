89e3ede3e581055a293323abadaf6295a9d2b9d1
Aalyah <jhonsonaalyah@gmail.com>
2023-11-14 00:16:39 -0500

    exercise 2

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
+// document.addEventListener("DOMContentLoaded", loadDOM)
+// function loadDOM(){ 
+//     displaySearch()
+// }
+// function displaySearch(){
+//   document.getElementById('btn').addEventListener ("click",()=> {
+//     fetch("superheroes.php")
+//     .then(response => response.text())
+//     .then( (data) =>alert(data))
+// });}
+
 document.addEventListener("DOMContentLoaded", loadDOM)
 function loadDOM(){ 
     displaySearch()
 }
 function displaySearch(){
   document.getElementById('btn').addEventListener ("click",()=> {
-    fetch("superheroes.php")
-    .then(response => response.text())
-    .then( (data) =>alert(data))
-});}
+    var input = document.getElementById("input").value;
+    let fData = new FormData();
+    let hed = new Headers();
+
+    fData.append('heroname', Sanitize(input));
+    let reqst = new Request("superheroes.php", {
+        method: 'POST',
+        headers: hed,   
+        mode: 'same-origin',
+        body: fData});
+    fetch(reqst)
+    .then( (response)=>
+         response.text()
+        )
+    .then( (data) =>{
+    console.log(data)
+    document.getElementById('result').innerHTML= data;})
+})};
+
+function Sanitize(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, " ");
+    return str.trim();
+}
